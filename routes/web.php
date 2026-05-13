19910281227962a0ead0e670ee01ce7c8d5f03bf
tsomorfaruk <tsomorfaruk@gmail.com>
2026-05-13 19:49:22 +0600

    Code updated

--- routes/web.php
+++ routes/web.php
@@ -5,14 +5,14 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
-    return redirect()->to('/custom-activity/v2');
+    return redirect()->to('/custom-activity/v4');
 });
 
-Route::get('/custom-activity/v2', function () {
+Route::get('/custom-activity/v4', function () {
     return redirect()->route('custom-activity.index');
 });
 
 
-Route::get('/custom-activity/v2/index.html', [CustomActivityController::class, 'index'])->name('custom-activity.index');
+Route::get('/custom-activity/v4/index.html', [CustomActivityController::class, 'index'])->name('custom-activity.index');
 Route::post('/custom-activity/upload-image', [CustomActivityController::class, 'upload_image'])->name('custom-activity.upload_image');
 
